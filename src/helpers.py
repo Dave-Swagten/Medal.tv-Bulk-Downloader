3871c123a843b0b48a58416eb66d86a61f8439d6
phyzical <5182053+phyzical@users.noreply.github.com>
2024-09-22 12:09:48 +0800

    refactor how config is passed around to reduce params

--- src/helpers.py
+++ src/helpers.py
@@ -1,33 +1,9 @@
-import json
 import datetime
 import re
+from config import CONFIG
 
-# Load configuration from JSON file
-def load_config():
-    config_file = 'config.json'
-    try:
-        with open(config_file, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        print(f"The configuration file '{config_file}' does not exist. Please refer to the README for setup instructions: https://github.com/Dave-Swagten/Medal.tv-Bulk-Downloader?tab=readme-ov-file#%EF%B8%8F-configuration")
-        
-        exit(1)
-    except json.JSONDecodeError:
-        print("Error parsing the configuration file. Ensure it is valid JSON format.")
-        exit(1)
-
-# Load cache file
-def load_cache(file_path):
-    if file_path != '':
-        try:
-            with open(file_path, 'r') as file:
-                return set(file.read().splitlines())
-        except FileNotFoundError:
-            print(f"Cache file not found: {file_path}. Creating a new one.")
-            open(file_path, 'w').close()  # Create an empty file
-    return set()  # Return an empty set if no file path is provided or if the file is empty
-
-def update_cache(content_id, file_path):
+def update_cache(content_id):
+    file_path = CONFIG['CACHE_FILE']
     if file_path != '':
         with open(file_path, 'a') as file:
             file.write(content_id + "\n")
